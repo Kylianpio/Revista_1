a33cd55a67e27150ab052ac69992aad3c80d43fd
Maily Chavez <mailychavez18@gmail.com>
2024-06-27 10:24:37 -0600

    nuevo

---
+++ java.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButtons = document.querySelectorAll('.toggle-content');
+    
+    toggleButtons.forEach(button => {
+    button.addEventListener('click', function() {
+    const content = this.nextElementSibling;
+    content.classList.toggle('show');
+    if (content.classList.contains('show')) {
+    this.textContent = 'Ver menos';
+    } else {
+    this.textContent = 'Ver más';
+    }
+    });
+    });
+    });
